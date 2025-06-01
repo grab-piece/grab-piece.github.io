367b822c66da5cd35b3766ffcde75f8ab41a729f
aalbert <services@bleu-azure.fr>
2025-06-01 08:30:27 +0200

    fix icons

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1748702742|6654011296';
+const CACHE_VERSION = '1748759100|267503493';
 /** @type {string} */
 const CACHE_PREFIX = 'Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
