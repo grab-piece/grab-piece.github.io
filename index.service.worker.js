089f4fb9937e912c42cdf881f13fd480b3480b6b
aalbert <services@bleu-azure.fr>
2025-06-05 14:23:39 +0200

    smarter strategy for loading

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749125768|90314733';
+const CACHE_VERSION = '1749126183|122422381';
 /** @type {string} */
 const CACHE_PREFIX = 'Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
