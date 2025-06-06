a5c65683518c7bd3469332e21a973c4af2243a93
aalbert <services@bleu-azure.fr>
2025-06-06 21:34:14 +0200

    move physic to another slower thread

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749149978|24566822';
+const CACHE_VERSION = '1749238415|545365580';
 /** @type {string} */
 const CACHE_PREFIX = 'Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
