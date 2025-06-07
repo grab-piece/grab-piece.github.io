9453f6a2dde7a8d7a22b04efba0e8fef14cf8c71
aalbert <services@bleu-azure.fr>
2025-06-07 10:19:42 +0200

    low processor mode

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1749238415|545365580';
+const CACHE_VERSION = '1749284362|252894990';
 /** @type {string} */
 const CACHE_PREFIX = 'Puzzle-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
